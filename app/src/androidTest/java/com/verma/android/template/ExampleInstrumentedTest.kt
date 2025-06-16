aa30ef1d003d6fad1dde61c348d210901031512e
Sourav Kumar Verma <logtosourav@gmail.com>
2025-06-16 14:38:28 +0530

    Upgrade Version

--- app/src/androidTest/java/com/verma/android/template/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/verma/android/template/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.verma.android.whatsupv2", appContext.packageName)
+        assertEquals("com.verma.android.template", appContext.packageName)
     }
 }
